af98a3e37e04495fd3324ece86f0e61b89f8a6e2
EduardoLeonel <edu.gomezcampos@unitec.edu>
2015-01-10 01:10:01 -0600

    Se centra texto

--- protected/views/admin/index.php
+++ protected/views/admin/index.php
@@ -17,7 +17,7 @@ $this->breadcrumbs = array(
                 <h3 class="panel-title"><i class="fa fa-users"></i>&nbsp;USUARIOS</h3>
             </div>
             <div class="panel-body">
-                <p class="st-section-desc">
+                <p class="st-section-desc text-centered">
                     Administración de los usuarios autorizados para el manejo del contenido del sitio.
                 </p>
 
@@ -33,7 +33,7 @@ $this->breadcrumbs = array(
                 <h3 class="panel-title"><i class="fa fa-newspaper-o"></i>&nbsp;PUBLICACIONES</h3>
             </div>
             <div class="panel-body">
-                <p class="st-section-desc">
+                <p class="st-section-desc text-centered">
                     Administración del contenido del sitio, creacion y edición de publicaciones para las
                     distintas secciones de contenido.
                 </p>
@@ -50,7 +50,7 @@ $this->breadcrumbs = array(
                 <h3 class="panel-title"><i class="fa fa-cubes"></i>&nbsp;SECCIONES</h3>
             </div>
             <div class="panel-body">
-                <p class="st-section-desc">Administración de las secciones de contenido disponibles en el sitio.</p>
+                <p class="st-section-desc text-centered">Administración de las secciones de contenido disponibles en el sitio.</p>
 
                 <div class="text-centered">
                     <a href="<?php echo Yii::app()->createUrl('section');?>" class="btn btn-success">Ingresar</a>
@@ -64,7 +64,7 @@ $this->breadcrumbs = array(
                 <h3 class="panel-title"><i class="fa fa-list"></i>&nbsp;INFORMACIÓN GENERAL</h3>
             </div>
             <div class="panel-body">
-                <p class="st-section-desc">Administración de la información general mostrada en la pantalla princiapl del sitio.</p>
+                <p class="st-section-desc text-centered">Administración de la información general mostrada en la pantalla princiapl del sitio.</p>
 
                 <div class="text-centered">
                     <a href="<?php echo $this->createUrl('editgeneralinfo');?>" class="btn btn-info">Editar</a>
