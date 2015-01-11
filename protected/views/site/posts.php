28f7b7b9e1e2b670850bd0a0f61d7b2fe02f3ec1
EduardoLeonel <edu.gomezcampos@unitec.edu>
2015-01-10 22:45:31 -0600

    format

--- protected/views/site/posts.php
+++ protected/views/site/posts.php
@@ -16,7 +16,9 @@ function dashed($name){
 		<div class="col-lg-12">
 			<div class="post-header">
 				<h2><?php echo $post->TITLE; ?></h2>
-				<div class="fb-share-button" style="float:right;" data-href="http://santalucia.tk/index.php/site/posts/<?php echo dashed($post->TITLE) . "_". $post->ID_POST;?>" data-layout="button_count"></div>
+				<div class="fb-share-button" style="float:right;"
+				data-href="http://santalucia.tk/index.php/site/posts/<?php echo dashed($post->TITLE) . "_". $post->ID_POST;?>"
+				data-layout="button_count"></div>
 				<i>
 					<?php
 					$date=date_create($post->CREATION_DATE);
@@ -25,17 +27,18 @@ function dashed($name){
 				</i>
 			</div>
 			<div class="post-content">
-				<?php echo $post->CONTENT; ?></div>
+				<?php echo $post->CONTENT; ?>
 			</div>
-		<?php endforeach;?>
-	</div>
+		</div>
+	<?php endforeach;?>
+</div>
 
 
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=265123660302034&version=v2.0";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=265123660302034&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
